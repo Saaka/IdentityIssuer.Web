56b44a394ba7175214b37575068345e48e739d52
Michal Sakwa <saka1988@gmail.com>
2020-03-29 00:52:14 +0100

    Added .env file to store local variables.

---
+++ IdentityIssuer.Web/ClientApp/src/App.jsx
@@ -0,0 +1,18 @@
+import "./assets/_style.scss";
+import React from 'react';
+
+function App(props) {
+    return (
+        <section className="hero has-background-black is-fullheight">
+            <div className="hero-body">
+                <div className="container">
+                    <figure className="image center">
+                        <img src="https://picsum.photos/256/128/" alt="Logo" style={{maxWidth: "256px"}}/>
+                    </figure>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export {App};
