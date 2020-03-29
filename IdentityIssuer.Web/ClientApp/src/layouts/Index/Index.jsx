01322500d0f1961a689441d81c9e91f3e07f2f21
Michal Sakwa <saka1988@gmail.com>
2020-03-29 21:39:06 +0200

    Added renderApp function to handle global routing.

--- IdentityIssuer.Web/ClientApp/src/layouts/Index/Index.jsx
+++ IdentityIssuer.Web/ClientApp/src/layouts/Index/Index.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import {Route, Redirect} from "react-router-dom";
+import {App} from "layouts/exports";
 import {RouteNames} from "routes/names";
 
 function Index(props) {
- 
+
     function renderApp() {
-        return(
-          <span>
-              <Route exact path={{RouteNames.Root}}/>
-          </span>  
+        return (
+            <span>
+              <Route exact path={RouteNames.Root} 
+                     render={(props) => <Redirect to={RouteNames.App} from={props.path} {...props}/>} />
+              <Route path={RouteNames.App} 
+                     render={(props) => <App {...props} />} />
+            </span>
         );
     }
-    
-    
-    return(
-      <div>
-          <h1>Welcome!</h1>
-      </div>  
+
+
+    return (
+        <div>
+            <h1>Welcome!</h1>
+        </div>
     );
 }
 
