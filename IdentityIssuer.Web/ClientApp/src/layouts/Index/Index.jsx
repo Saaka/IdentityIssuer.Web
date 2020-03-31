474a1b895a6a29905d4eeb3cf91894913dfaa4f2
Michal Sakwa <saka1988@gmail.com>
2020-03-31 23:04:59 +0200

    Added base login view.

--- IdentityIssuer.Web/ClientApp/src/layouts/Index/Index.jsx
+++ IdentityIssuer.Web/ClientApp/src/layouts/Index/Index.jsx
@@ -2,25 +2,30 @@ import React, {useState, useEffect} from 'react';
 import {Route, Redirect} from "react-router-dom";
 import {App} from "layouts/exports";
 import {RouteNames} from "routes/names";
+import {Loader} from "components/exports";
+import {Login} from "views/exports";
 
 function Index(props) {
 
     const [isLoading, setIsLoading] = useState(true);
-    
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsLoading(false);
         }, 1000);
         return () => clearTimeout(timer);
-    },[]);
+    }, []);
 
     function renderApp() {
         return (
             <span>
-              <Route exact path={RouteNames.Root}
-                     render={(props) => <Redirect to={RouteNames.App} from={props.path} {...props}/>}/>
-              <Route path={RouteNames.App}
-                     render={(props) => <App {...props} />}/>
+                <Route exact
+                       path={RouteNames.Root}
+                       render={(props) => <Redirect to={RouteNames.App} from={props.path} {...props}/>}/>
+                <Route path={RouteNames.Login}
+                       render={(props) => <Login {...props} />}/>
+                <Route path={RouteNames.App}
+                       render={(props) => <App {...props} />}/>
             </span>
         );
     }
@@ -29,7 +34,7 @@ function Index(props) {
         return (
             <div className="hero has-background-gradient is-fullheight">
                 <div className="hero-body center">
-                    Loader!
+                    <Loader/>
                 </div>
             </div>
         );
---
+++ IdentityIssuer.Web/ClientApp/src/views/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.scss";
+
+function Login(props) {
+
+
+    return (
+        <div className="container">
+            <h1>Login!</h1>
+        </div>
+    );
+}
+
+export {Login};
