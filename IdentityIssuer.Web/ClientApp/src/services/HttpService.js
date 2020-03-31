c2bc2f025b9ae408b206b90ca14f1eb67cc271a6
Michal Sakwa <saka1988@gmail.com>
2020-03-31 23:54:06 +0200

    Updated HttpService with tenant header.

--- IdentityIssuer.Web/ClientApp/src/services/HttpService.js
+++ IdentityIssuer.Web/ClientApp/src/services/HttpService.js
@@ -1,48 +1,44 @@
-import { ConfigService } from "Services";
+import {ConfigService} from "Services";
 import Axios from "axios";
 
 class HttpService {
 
-    constructor(baseAddress) {
+    constructor(baseAddress, baseTenant) {
         this.configService = new ConfigService();
-        this.baseAddress =  baseAddress || this.configService.ApiUrl;
+        this.baseAddress = baseAddress || this.configService.ApiUrl;
+        this.baseTenant = baseTenant;
     };
 
-    _axios = null;
-    get axios() {
-        if(!this._axios)
-            this._axios = this.createAxios(this.baseAddress);
-
-        return this._axios;
-    }
-
-    createAxios = (baseAddress) => {
-        return Axios.create({
-            baseURL: `${baseAddress}/`
+    get = (address, tenant) => {
+        return Axios({
+            method: "get",
+            url: `${this.baseAddress}/` + address,
+            headers: {
+                "X-Tenant-Code": tenant || this.baseTenant
+            }
+        }).catch(err => {
+            if (err.response)
+                throw err.response.data;
+            else
+                throw err.message;
         });
     };
 
-    get = (address) => {
-        return this.axios
-            .get(address)
-            .catch(err => {
-                if (err.response)
-                    throw err.response.data;
-                else
-                    throw err.message;
-            });
-    };
-
-    post = (address, data) => {
-        return this.axios
-            .post(address, data)
-            .catch(err => {
-                if (err.response)
-                    throw err.response.data;
-                else
-                    throw err.message;
-            });
+    post = (address, data, tenant) => {
+        return Axios({
+            method: "post",
+            url: `${this.baseAddress}` + address,
+            data: data,
+            headers: {
+                "X-Tenant-Code": tenant || this.baseTenant
+            }
+        }).catch(err => {
+            if (err.response)
+                throw err.response.data;
+            else
+                throw err.message;
+        });
     };
 }
 
-export { HttpService }
+export {HttpService}
