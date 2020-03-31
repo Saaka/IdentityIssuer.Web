9040f2418ed0e7d2a14e836cd6d6e76313c97135
Michal Sakwa <saka1988@gmail.com>
2020-03-31 23:32:30 +0200

    Added HttpService and ConfigService.

--- IdentityIssuer.Web/ClientApp/src/Services.js
+++ IdentityIssuer.Web/ClientApp/src/Services.js
@@ -1,3 +1,5 @@
 export {AuthService} from "services/AuthService";
+export {ConfigService} from "services/ConfigService";
 export {Constants} from "services/Constants";
+export {HttpService} from "services/HttpService";
 export {UserTokenService} from "services/UserTokenService";
---
+++ IdentityIssuer.Web/ClientApp/src/services/ConfigService.js
@@ -0,0 +1,5 @@
+export class ConfigService {
+    constructor(){
+        this.ApiUrl = process.env.REACT_APP_API_URL;
+    }
+}
