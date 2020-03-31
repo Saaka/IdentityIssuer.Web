e4293920b0e697f7b1f90d5939773cb96111f1eb
Michal Sakwa <saka1988@gmail.com>
2020-04-01 01:30:17 +0200

    Display title on Dashboard.

--- IdentityIssuer.Web/ClientApp/src/views/Dashboard/Dashboard.jsx
+++ IdentityIssuer.Web/ClientApp/src/views/Dashboard/Dashboard.jsx
@@ -6,10 +6,15 @@ function Dashboard(props) {
     return (
         <section className="hero has-background-gradient is-fullheight">
             <div className="hero-body">
-                <div className="container center">
-                    <figure className="image is-128x128 has-border">
-                        <img src="https://picsum.photos/128/128/" alt="Logo" style={{maxWidth: "256px"}} />
-                    </figure>
+                <div className="container login-container center">
+                    <div>
+                        <h1 className="is-size-1">Welcome to Identity Issuer Web UI</h1>
+                    </div>
+                    <div>
+                        <figure className="image is-128x128 has-border">
+                            <img src="https://picsum.photos/128/128/" alt="Logo" style={{maxWidth: "256px"}}/>
+                        </figure>
+                    </div>
                 </div>
             </div>
         </section>
