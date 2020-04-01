334527d8964852cf91025075483b4a3f52a9c845
Michal Sakwa <saka1988@gmail.com>
2020-04-01 23:53:04 +0200

    Renamed dashboard class name.

--- IdentityIssuer.Web/ClientApp/src/views/Dashboard/Dashboard.jsx
+++ IdentityIssuer.Web/ClientApp/src/views/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard(props) {
     return (
         <section className="hero has-background-gradient is-fullheight">
             <div className="hero-body">
-                <div className="container login-container center">
+                <div className="container dashboard-container center">
                     <div>
                         <h1 className="is-size-1">Welcome to Identity Issuer Web UI</h1>
                     </div>
