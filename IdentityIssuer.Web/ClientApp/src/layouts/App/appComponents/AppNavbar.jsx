8266ea94e9b995de4924f260a0abc54e25a20997
Michal Sakwa <saka1988@gmail.com>
2020-04-05 16:44:57 +0200

    Added working dropdown menu for mobile.

--- IdentityIssuer.Web/ClientApp/src/layouts/App/appComponents/AppNavbar.jsx
+++ IdentityIssuer.Web/ClientApp/src/layouts/App/appComponents/AppNavbar.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AppNavbar.scss";
 import {RouteNames} from "routes/names";
 
 function AppNavbar(props) {
+    const [isMenuActive, setActiveMenu] = useState(false);
 
     function logout() {
         props.history.replace(RouteNames.Logout)
     }
 
+    const toggleNavbarMenu = () => setActiveMenu(prevState => !prevState);
+
+    const menuClasses = () => isMenuActive ? "navbar-menu is-active": "navbar-menu";
+    const burgerClasses = () => isMenuActive ? "navbar-burger burger is-active": "navbar-burger burger";
+    
     return (
         <nav className="navbar is-transparent is-primary" role="navigation" aria-label="main site navigation">
             <div className="container">
                 <div className="navbar-brand">
                     <a className="navbar-item">IdentityIssuer</a>
 
-                    <a className="navbar-burger burger" data-target="app-navbar-menu" role="button" aria-label="dropdown menu" aria-expanded="false">
+                    <a className={burgerClasses()} 
+                       onClick={toggleNavbarMenu}
+                       role="button" aria-label="dropdown menu" aria-expanded="false">
                         <span aria-hidden="true"/>
                         <span aria-hidden="true"/>
                         <span aria-hidden="true"/>
                     </a>
                 </div>
-                <div className="navbar-menu" id="app-navbar-menu">
+                <div className={menuClasses()} id="app-navbar-menu">
                     <div className="navbar-end">
                         <div className="navbar-item">
                             <a className="button" onClick={logout}>Log out</a>
